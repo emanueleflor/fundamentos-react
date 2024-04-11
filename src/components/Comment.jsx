57c65fc37b903bf6a637633d8efd84a6347ac73d
Emanuele Flor da Rosa <emanueleflordarosa@Brunos-MacBook-Air.local>
2024-04-11 14:43:37 -0300

    comments style

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import styles from "./Comment.module.css"
+import { Trash, ThumbsUp} from "phosphor-react"
+export function Comment(){
+    return (
+        <div className={styles.comment}>
+            <img src="https:github.com/emanueleflor.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Leli Flor</strong>
+                            <time title="11 de abriL as 11:42 "dateTime="2024-11-04 11:42:05">Cerca de 1 hora atrás</time>
+                        </div>
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    
+                    <p>
+                        Muito bom Devon, parabéns!
+                    </p>
+                </div>
+
+               <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button> 
+               </footer>
+            </div>
+        </div>
+    )
+}
