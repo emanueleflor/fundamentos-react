d1c384cc3e82f858fd01821d4f021c0aeb9df3ba
Emanuele Flor da Rosa <emanuele.rosa@startse.com>
2024-04-12 10:05:18 -0300

    adding properties on posts

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,38 @@ import styles from './App.module.css'
 
 import "./global.css"
 
+const posts =[
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/emanuelerosa.png',
+      name: 'Emanuele Flor',
+      role: 'Software Engineer'
+    },
+    content: [
+      {type: 'paragraph', text: 'Fala Galera!'},
+      {type: 'paragraph', text: 'Acabei de postar uma nova viodeo aula no meu canal!'},
+      {type: 'paragraph', text: 'Vem conferir!'},
+      {type: 'link', url: 'jane.desing/doctorcare'}
+    ],
+    publishedAt: new Date('2021-03-11 16:12:00').toISOString()
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/emanuelerosa.png',
+      name: 'Emanuele Flor',
+      role: 'Educator'
+    },
+    content: [
+      {type: 'paragraph', text: 'Fala Galera!'},
+      {type: 'paragraph', text: 'Acabei de postar uma nova viodeo aula no meu canal!'},
+      {type: 'paragraph', text: 'Vem conferir!'},
+      {type: 'link', url: 'jane.desing/doctorcare'}
+    ],
+    publishedAt: new Date('2024-04-11 08:12:00')
+  }
+]
 
 export function App() {
   return (
@@ -14,14 +46,15 @@ export function App() {
       <div className={styles.wrapper}>
        <Sidebar />
         <main>
-          <Post
-            author='LeliFlor'
-            content='Lorem ipsum'
-          />
-          <Post
-            author='Diego'
-            content='Lorem ipsum'
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })} 
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,45 +1,83 @@
+import { format, formatDistanceToNow } from "date-fns"
+import { ptBR } from 'date-fns/locale';
 import { Avatar } from "./Avatar"
 import { Comment } from "./Comment"
 import styles from "./Post.module.css"
+import { useState } from "react";
 
-export function Post () {
+
+export function Post({ author, publishedAt, content }) {
+
+    const [comments, setComments] = useState([
+        1,
+        2,
+        3,
+    ])
+
+    const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm", {
+        locale: ptBR,
+    })
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    })
+
+    function handleCreateNewComment() {
+        event.preventDefault()
+
+        set.comments([...comments, comments.length + 1])
+        console.log(comments)
+    }
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https:github.com/emanueleflor.png" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Leli Flor</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title="11 de abriL as 11:42 "dateTime="2024-11-04 11:42:05">Publicado ha 1 hora</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt}>
+                    {publishedDateRelativeToNow}
+                </time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galera</p>
-                <p>Aula no portifolio</p>
-                <p> <a href="">jane.desing/doctor.care</a></p>
-                <p> <a href="">#novoprojeto #nlw #rocketseat</a></p>
+                {content.map(line => {
+                    switch (line.type) {
+                        case 'paragraph':
+                            return <p>{line.text}</p>
+                            break;
+
+                        case 'link':
+                            return <p><a href="#">{line.url}</a></p>
+                        default:
+                            break;
+                    }
+                })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
                 <textarea
                     placeholder="Deixe um comentário"
                 />
-                
+
                 <footer>
                     <button type="submit">Publicar</button>
                 </footer>
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => (
+                     <Comment />
+                ))}
             </div>
+
+
         </article>
     )
 }
